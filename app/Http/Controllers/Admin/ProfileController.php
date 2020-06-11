2301044ab29c4510f25ad98c6cae786442ea3090
”shiho.nakano” <nknsh71.71@gmail.com>
2020-06-11 13:05:25 +0000

    PHP/Laravel14 課題

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+//
+use App\Profile;
 
 class ProfileController extends Controller
 {
@@ -13,8 +15,19 @@ class ProfileController extends Controller
         return view('admin.profile.create');
     }
 
-    public function create()
+    public function create(Request $request)
     {
+          // 以下を追記
+      // Varidationを行う
+      $this->validate($request, Profile::$rules);
+      $profile = new Profile;
+      $form = $request->all();
+      
+      
+      // データベースに保存する
+      $profile->fill($form);
+      $profile->save();
+      
         return redirect('admin/profile/create');
     }
 
@@ -27,4 +40,5 @@ class ProfileController extends Controller
     {
         return redirect('admin/profile/edit');
     }
+    
 }
