1a3ee040ca31354dd7211a3c5b45a59babaef03c
”shiho.nakano” <nknsh71.71@gmail.com>
2020-06-21 09:05:43 +0000

    PHP/Laravel19 heroku

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,7 +23,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // 以下を追記
+        if (\App::environment('production')) {
+            \URL::forceScheme('https');
+        }
         \URL::forceScheme('https');
     }
 }
